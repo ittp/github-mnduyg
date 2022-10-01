59212de0f685fa08abc719ff9954c35a60b22942
ittp <tp@ittps.ru>
2022-10-01 10:01:03 +0000

    Update 6 files

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 // Import our custom CSS
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 
 // Import only the Bootstrap components we need
-import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
+import { Util, Dropdown, Offcanvas, Popover } from "bootstrap";
 
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+  new Popover(popover);
+});
 
+const axios = require("axios");
 
-
+const logo = require("./assets/spb.svg");
