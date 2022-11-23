ff3e4fe526bf5b1fdeffd311273fe530fb58ea22
ittp <61617660+ittp@users.noreply.github.com>
2022-11-23 20:44:23 +0300

    3 files modified, 1 added

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,11 @@ import '../scss/styles.scss';
 // Import only the Bootstrap components we need
 import { Util, Dropdown, Offcanvas, Popover } from 'bootstrap';
 
+import axios from 'axios';
+
+import { useSWR } from 'swr';
+
 // Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new Popover(popover)
-  })
+document.querySelectorAll('[data-bs-toggle="popover"]').forEach((popover) => {
+  new Popover(popover);
+});
